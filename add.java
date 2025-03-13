cb6bbb11f2e72c746dbe2fb88a44b74629726d8b
Sameerkhan7610 <sameerkhanbeswa901@gmail.com>
2025-03-13 12:38:02 +0530

    MY first commit

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add{
+    public static void main(String[] args) {
+        int n1=25; int n2=15;
+
+        int sum =n1+n2;
+
+        System.out.println("the sum of numbers is:"+sum);
+    }
+}
